30085f2c089daf6591be3e679d2d5437541b3a9f
Ewout Merckx <ewout.merckx@kahosl.be>
2014-04-10 17:45:22 +0200

    NuGet packages added and assembly info changed
    The NuGet packages of FileSystem and ModernImageLibrary have been added
    and the assembly info has been changed

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UI")]
+[assembly: AssemblyTitle("Filedrops FileSystem UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("xplore+")]
-[assembly: AssemblyProduct("UI")]
+[assembly: AssemblyProduct("Filedrops FileSystem UI")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
